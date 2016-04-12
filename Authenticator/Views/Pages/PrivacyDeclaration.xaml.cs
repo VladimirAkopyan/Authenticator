23160cc7e6e347803091572c52658494d8cce36c
Hoithebest <roeleman15@hotmail.com>
2016-04-12 19:58:07 +0200

    Localized PrivacyDeclarations

--- Authenticator/Views/Pages/PrivacyDeclaration.xaml.cs
+++ Authenticator/Views/Pages/PrivacyDeclaration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Globalization;
+using System.IO;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -10,11 +11,21 @@ namespace Authenticator_for_Windows.Views.Pages
     /// </summary>
     public sealed partial class PrivacyDeclaration : Page
     {
+        private const string FILENAME_STRUCTURE = "Resources/PrivacyDeclaration.{0}.html";
+        private const string DEFAULT_LANGUAGE = "en-US";
+
         public PrivacyDeclaration()
         {
             InitializeComponent();
 
-            string html = File.ReadAllText("Resources/PrivacyDeclaration.html");
+            string fileName = string.Format(FILENAME_STRUCTURE, DEFAULT_LANGUAGE);
+
+            if (File.Exists(string.Format(FILENAME_STRUCTURE, CultureInfo.CurrentUICulture.Name)))
+            {
+                fileName = string.Format(FILENAME_STRUCTURE, CultureInfo.CurrentUICulture.Name);
+            }
+
+            string html = File.ReadAllText(fileName);
 
             WebView.NavigateToString(html);
         }
