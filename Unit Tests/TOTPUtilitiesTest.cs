4c9928fea06bfb585cf96c79ab48b87d25d67a33
Hoithebest <roeleman15@hotmail.com>
2016-06-11 16:10:52 +0200

    Added a setup for unit testing services

---
+++ Unit Tests/TOTPUtilitiesTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Domain;
+using Domain.Utilities;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMicrosoft()
+        {
+            string input = "otpauth://totp/Microsoft:user@hotmail.com?secret=2FA662OCY6FLRKY5&issuer=Microsoft";
+            Account account = TOTPUtilities.UriToAccount(input);
+
+            Validate("user@hotmail.com", "2FA662OCY6FLRKY5", "Microsoft", account);
+        }
+
+        private void Validate(string username, string secret, string service, Account account)
+        {
+            Assert.IsNotNull(account);
+            Assert.AreEqual(username, account.Username);
+            Assert.AreEqual(secret, account.Secret);
+            Assert.AreEqual(service, account.Service);
+        }
+    }
+}
