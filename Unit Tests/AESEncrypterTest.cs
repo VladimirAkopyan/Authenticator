ad5ec313dc53032bfeda33a6a853e39e5660341d
Hoithebest <roeleman15@hotmail.com>
2016-07-14 13:34:26 +0200

    Added a unit test for AESEncrypter

---
+++ Unit Tests/AESEncrypterTest.cs
@@ -0,0 +1,35 @@
+﻿using Encryption;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class AESEncrypterTest
+    {
+        [TestMethod]
+        public void TestEncryptAndDecrypt()
+        {
+            string input = "Test";
+
+            AESEncrypter encrypter = GetAESEncrypter();
+            string encrypted = encrypter.Encrypt(input);
+            string decrypted = encrypter.Decrypt(encrypted);
+
+            Assert.AreEqual(input, decrypted);
+        }
+
+        private AESEncrypter GetAESEncrypter()
+        {
+            return new AESEncrypter()
+            {
+                Password = "TestPassword",
+                Salt = "TestSalt"
+            };
+        }
+    }
+}
