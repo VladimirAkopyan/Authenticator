ca916480db073b4c35bee318978a73bd34a6460b
Animesh <hello@animesh.ltd>
2019-06-07 02:28:20 +0100

    Refactor settings UI; codebehind pending

---
+++ Authenticator/Views/Settings/GeneralSettings.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Authenticator.Views.Settings
+{
+    public sealed partial class GeneralSettings : UserControl
+    {
+        public GeneralSettings()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NTPTimeoutSliderValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+
+        private void EnableTimeSyncToggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void ClipboardAvailabilityChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
