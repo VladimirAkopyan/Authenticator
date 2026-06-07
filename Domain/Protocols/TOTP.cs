5a12bb5797e4118f6d3734e528043caf09e0e80f
Vladimir Akopyan <vladimir@akopyan.me>
2026-06-07 19:25:18 +0100

    Support configurable TOTP digit counts

--- Domain/Protocols/TOTP.cs
+++ Domain/Protocols/TOTP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,10 @@ namespace Domain.Protocols
 {
     public class TOTP
     {
-        public const byte DIGITS = 6;
+        public const byte DEFAULT_DIGITS = 6;
+        public const byte DIGITS = DEFAULT_DIGITS;
+        public const byte MIN_DIGITS = 1;
+        public const byte MAX_DIGITS = 9;
         public const long EPOCH = 621355968000000000;
         public const int INTERVAL = 30000;
     }
